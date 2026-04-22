47de5a9d3322146673f5c23d010a3481178e20e5
bituan <113036994+bituann@users.noreply.github.com>
2026-04-22 05:53:05 +0100

    other config files

---
+++ src/test/java/com/bituan/intelligence_query_engine/IntelligenceQueryEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bituan.intelligence_query_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceQueryEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
