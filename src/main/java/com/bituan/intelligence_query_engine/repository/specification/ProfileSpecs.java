4e21041fb485f569cbfda1f821081ad7670ba90c
bituan <113036994+bituann@users.noreply.github.com>
2026-04-22 05:50:34 +0100

    fix bugs with property access & validation

--- src/main/java/com/bituan/intelligence_query_engine/repository/specification/ProfileSpecs.java
+++ src/main/java/com/bituan/intelligence_query_engine/repository/specification/ProfileSpecs.java
@@ -5,38 +5,38 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ProfileSpecs {
     public Specification<Profile> isGender (String gender) {
-        return (root, query, builder) -> gender == null ? null : builder.equal(root.get("gender"), gender);
+        return (root, query, builder) -> gender == null || gender.isBlank() ? null : builder.equal(root.get("gender"), gender);
     }
 
     public Specification<Profile> isAgeGroup (String ageGroup) {
-        return (root, query, builder) -> ageGroup == null ? null : builder.equal(root.get("age_group"), ageGroup);
+        return (root, query, builder) -> ageGroup == null || ageGroup.isBlank() ? null : builder.equal(root.get("ageGroup"), ageGroup);
     }
 
     public Specification<Profile> isCountryId (String countryId) {
-        return (root, query, builder) -> countryId == null ? null : builder.equal(root.get("country_id"), countryId);
+        return (root, query, builder) -> countryId == null || countryId.isBlank() ? null : builder.equal(root.get("countryId"), countryId);
     }
 
     public Specification<Profile> ageLessThan (Integer age) {
-        return (root, query, builder) -> age == null ? null : builder.lessThan(root.get("age"), age);
+        return (root, query, builder) -> age == null ? null : builder.lessThanOrEqualTo(root.get("age"), age);
     }
 
     public Specification<Profile> ageGreaterThan (Integer age) {
-        return (root, query, builder) -> age == null ? null : builder.greaterThan(root.get("age"), age);
+        return (root, query, builder) -> age == null ? null : builder.greaterThanOrEqualTo(root.get("age"), age);
     }
 
     public Specification<Profile> genderProbabilityLessThan (Double genderProbability) {
-        return (root, query, builder) -> genderProbability == null ? null : builder.lessThan(root.get("gender_probability"), genderProbability);
+        return (root, query, builder) -> genderProbability == null ? null : builder.lessThanOrEqualTo(root.get("genderProbability"), genderProbability);
     }
 
     public Specification<Profile> genderProbabilityGreaterThan (Double genderProbability) {
-        return (root, query, builder) -> genderProbability == null ? null : builder.greaterThan(root.get("gender_probability"), genderProbability);
+        return (root, query, builder) -> genderProbability == null ? null : builder.greaterThanOrEqualTo(root.get("genderProbability"), genderProbability);
     }
 
     public Specification<Profile> countryProbabilityLessThan (Double countryProbability) {
-        return (root, query, builder) -> countryProbability == null ? null : builder.lessThan(root.get("country_probability"), countryProbability);
+        return (root, query, builder) -> countryProbability == null ? null : builder.lessThanOrEqualTo(root.get("countryProbability"), countryProbability);
     }
 
     public Specification<Profile> countryProbabilityGreaterThan (Double countryProbability) {
-        return (root, query, builder) -> countryProbability == null ? null : builder.greaterThan(root.get("country_probability"), countryProbability);
+        return (root, query, builder) -> countryProbability == null ? null : builder.greaterThanOrEqualTo(root.get("countryProbability"), countryProbability);
     }
 }
