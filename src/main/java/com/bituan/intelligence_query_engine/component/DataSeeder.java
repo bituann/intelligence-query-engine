400c4d06ad3907c25e59e364d07c2b2d1c6b9e43
bituan <113036994+bituann@users.noreply.github.com>
2026-04-23 21:55:57 +0100

    use hosted json file rather than encoded json

--- src/main/java/com/bituan/intelligence_query_engine/component/DataSeeder.java
+++ src/main/java/com/bituan/intelligence_query_engine/component/DataSeeder.java
@@ -6,39 +6,53 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
-import java.util.Base64;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class DataSeeder implements CommandLineRunner {
 
-    @Value("${SEED_DATA}")
-    private String encodedJson;
+    @Value("${SEED_DATA_URL}")
+    private String url;
 
     private final ObjectMapper objectMapper;
     private final ProfileRepository profileRepository;
 
     @Override
     public void run(String... args) {
-        if (profileRepository.count() != 0) {
-            return;
-        }
-
-        if (encodedJson != null && !encodedJson.isBlank()) {
-            byte[] decodedBytes = Base64.getDecoder().decode(encodedJson);
-
-            String json = new String(decodedBytes);
-
-            try {
-                List<Profile> users = objectMapper.readValue(json, new TypeReference<>(){});
-                profileRepository.saveAll(users);
-            } catch (Exception e) {
-                throw new ServerException("Error initializing database");
+        try {
+            if (profileRepository.count() != 0) {
+                return;
             }
+
+            byte[] jsonBytes = new RestTemplate().getForObject(url, byte[].class);
+
+            if (jsonBytes == null) {
+                throw new Exception();
+            }
+
+            JsonNode json = objectMapper.readTree(new String(jsonBytes)).get("profiles");
+
+            if (json == null || !json.isArray()) {
+                throw new Exception("Invalid JSON structure: 'profiles' field missing or not an array");
+            }
+
+            List<Profile> users = objectMapper.convertValue(json, new TypeReference<>(){});
+            profileRepository.saveAll(users);
+
+        } catch (Exception e) {
+            throw new ServerException(e.getMessage());
         }
     }
 }
