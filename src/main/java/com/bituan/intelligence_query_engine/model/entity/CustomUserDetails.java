fb14f1a45a680097fa6480842a9132b912f1e900
bituan <113036994+bituann@users.noreply.github.com>
2026-04-28 12:39:45 +0100

    feat: add new entities for user and tokens

---
+++ src/main/java/com/bituan/intelligence_query_engine/model/entity/CustomUserDetails.java
@@ -0,0 +1,26 @@
+package com.bituan.intelligence_query_engine.model.entity;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public record CustomUserDetails (User user) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Stream.of(user.getRole()).map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ src/main/java/com/bituan/intelligence_query_engine/model/entity/User.java
@@ -0,0 +1,43 @@
+package com.bituan.intelligence_query_engine.model.entity;
+
+import com.bituan.intelligence_query_engine.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(algorithm = org.hibernate.id.uuid.UuidVersion7Strategy.class)
+    private UUID id;
+
+    @Column(unique = true)
+    private String githubId;
+
+    private String username;
+    private String email;
+    private String avatarUrl;
+    private UserRole role;
+
+    @JsonProperty("is_active")
+    private boolean isActive;
+    private ZonedDateTime lastLoginAt;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private ZonedDateTime createdAt;
+}
