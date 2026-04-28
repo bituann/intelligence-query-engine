4b75989a44ea19a1312fd60f94c4be5c676fcc4e
bituan <113036994+bituann@users.noreply.github.com>
2026-04-28 16:57:12 +0100

    refactor(profiles): fix imports due to refactor

--- src/main/java/com/bituan/intelligence_query_engine/service/profile/ProfileService.java
+++ src/main/java/com/bituan/intelligence_query_engine/service/profile/ProfileService.java
@@ -1,6 +1,6 @@
 package com.bituan.intelligence_query_engine.service.profile;
 
-import com.bituan.intelligence_query_engine.model.ProfilesResponse;
+import com.bituan.intelligence_query_engine.model.response.ProfilesResponse;
 import com.bituan.intelligence_query_engine.model.request.GetProfilesRequestQueryModel;
 import com.bituan.intelligence_query_engine.model.response.ProfileResponse;
 
