ca48443ca416ab2b391e22c72dcb2a48f937f346
bituan <113036994+bituann@users.noreply.github.com>
2026-04-28 12:41:56 +0100

    refactor: specify table name for profiles

--- src/main/java/com/bituan/intelligence_query_engine/model/Profile.java
+++ src/main/java/com/bituan/intelligence_query_engine/model/Profile.java
@@ -1,9 +1,6 @@
 package com.bituan.intelligence_query_engine.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +10,7 @@ import org.hibernate.annotations.UuidGenerator;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
+@Table(name = "profiles")
 @Entity
 @Getter
 @AllArgsConstructor
