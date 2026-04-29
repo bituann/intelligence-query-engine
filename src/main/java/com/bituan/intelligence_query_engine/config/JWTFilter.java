58e94401f4ae961fed319d04b21783e8b304e973
bituan <113036994+bituann@users.noreply.github.com>
2026-04-29 22:29:26 +0100

    refactor(auth): secure all auth endpoints except github oauth

--- src/main/java/com/bituan/intelligence_query_engine/config/JWTFilter.java
+++ src/main/java/com/bituan/intelligence_query_engine/config/JWTFilter.java
@@ -33,7 +33,7 @@ public class JWTFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String reqPath = request.getServletPath();
 
-        return reqPath.startsWith("/auth") && !reqPath.equalsIgnoreCase("/auth/me");
+        return reqPath.startsWith("/auth/github");
     }
 
     @Override
